ac3fc1aba4d8719c75351bd53ea0d188916ec145
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-12-09 17:11:57 -0500

    Ajout du maxlength=20 et affichage des erreurs dans le formulaire de création

--- resources/views/cellar/create.blade.php
+++ resources/views/cellar/create.blade.php
@@ -3,20 +3,29 @@
 
 {{-- Formulaire d'ajout d'un nouveau cellier --}}
 @section('content')
-    <section class="p-4 pt-2" aria-label="Ajout d'un nouveau cellier">
-        {{-- Insertion du composant d'en-tête de page --}}
-        <x-page-header title="Créer un cellier" />
-        <div class="mt-6">
-            <form 
-                action="{{ route('cellar.store') }}" 
-                method="POST" 
-                class="flex flex-col gap-4"
-                aria-label="Formulaire de création de cellier"
-            >
-                @csrf
-                <x-input label="Nom du cellier" name="nom" type="text" placeholder="Entrez le nom du cellier" />
-                <x-primary-btn label="Ajouter le cellier" type="submit" />
-            </form>
-        </div>
-    </section>
+<section class="p-4 pt-2" aria-label="Ajout d'un nouveau cellier">
+    {{-- Insertion du composant d'en-tête de page --}}
+    <x-page-header title="Créer un cellier" />
+
+    {{-- Affichage des erreurs de validation --}}
+    @if ($errors->any())
+    <div class="bg-red-100 text-red-700 p-3 rounded text-sm mb-4">
+        @foreach ($errors->all() as $error)
+        <p>• {{ $error }}</p>
+        @endforeach
+    </div>
+    @endif
+
+    <div class="mt-6">
+        <form
+            action="{{ route('cellar.store') }}"
+            method="POST"
+            class="flex flex-col gap-4"
+            aria-label="Formulaire de création de cellier">
+            @csrf
+            <x-input label="Nom du cellier" name="nom" type="text" maxlength="20" placeholder="Entrez le nom du cellier" value="{{ old('nom', $cellier->nom ?? '') }}" />
+            <x-primary-btn label="Ajouter le cellier" type="submit" />
+        </form>
+    </div>
+</section>
 @endsection
